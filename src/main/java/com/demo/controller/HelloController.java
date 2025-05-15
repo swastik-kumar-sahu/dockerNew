ffbc3e2a191fb1620a5f73d6846aa815a73464fc
swastik-kumar-sahu <131697808+swastik-kumar-sahu@users.noreply.github.com>
2025-05-16 01:27:41 +0530

    Update HelloController.java

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
-    @GetMapping
+    @GetMapping("/message")
 	public String getMessage() {
 		return "krishna";
 	}
